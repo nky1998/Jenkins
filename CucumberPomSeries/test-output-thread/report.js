4afdecb481c6344de04b68474a716319757abb27
Nitish <nky1998@gmail.com>
2021-09-12 23:37:42 +0530

    Adding Cucumber Project

---
+++ CucumberPomSeries/test-output-thread/report.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "e2967c5e-fe0c-4ef9-9a19-bac54efedb0f",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1631469085497,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1631469100340,
+    "className": "failed"
+  },
+  {
+    "id": "a192f41b-0404-49e6-be62-e54e00779955",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1631469051609,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1631469070547,
+    "className": "passed"
+  },
+  {
+    "id": "37dfd6d0-32ff-42ae-b59b-b1af971aa952",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1631469070570,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1631469085483,
+    "className": "passed"
+  },
+  {
+    "id": "9c3e7149-6243-4cc1-82e0-ca4c2bed5c56",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1631469030096,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1631469051566,
+    "className": "passed"
+  },
+  {
+    "id": "3cabfa9c-3f2e-4832-9402-5722673aec87",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1631469100379,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1631469117254,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
